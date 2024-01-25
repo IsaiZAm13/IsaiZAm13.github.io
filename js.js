5b4570f8ed187bc98d30e3ab18627fee40a177fd
IsaiZAm13 <155692854+IsaiZAm13@users.noreply.github.com>
2024-01-24 18:11:36 -0600

    Add files via upload

--- js.js
+++ js.js
@@ -1,6 +1,5 @@
 function validacionExpre() {
     var emailInput = document.getElementById('email');
-    var emailHelp = document.getElementById('emailayuda');
     var emailvalidacion = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
 
     if (!emailvalidacion.test(emailInput.value)) {
