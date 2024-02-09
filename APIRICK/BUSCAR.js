f4b47bb62c5ed92b2f0909a224ac6ff4fdf6eba0
IsaiZAm13 <155692854+IsaiZAm13@users.noreply.github.com>
2024-02-08 18:23:20 -0600

    Se añadieron nuevas funciones
    Se añadió la función de generar listas al igual que la función de realizar búsqueda mediante nombres

--- APIRICK/BUSCAR.js
+++ APIRICK/BUSCAR.js
@@ -35,6 +35,7 @@ function personajerandome() {
 function buscarpersonaje() {
     const PersonajeID = document.getElementById('characterName').value;
     
+    
     fetch("https://rickandmortyapi.com/api/character/" + PersonajeID)
         .then(response => {
             if (!response.ok) {
@@ -58,7 +59,82 @@ function buscarpersonaje() {
 
            
             characterCardsContainer.appendChild(clone);
+      
+
         })
         .catch(error => window.alert(`Error fetching character: ${error}`));
 }
+
+function GenerarPersonajes() {
+    fetch("https://rickandmortyapi.com/api/character")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const characterCardsContainer = document.getElementById('characterCards');
+            const template = document.getElementById('characterCardTemplate');
+            
+            
+            characterCardsContainer.innerHTML = '';
+
+            
+            for (let i = 0; i < 20; i++) {
+                const character = data.results[i];
+                const clone = document.importNode(template.content, true);
+                
+               
+                clone.querySelector("[data-id='image']").src = character.image;
+                clone.querySelector("[data-id='title']").textContent = character.name;
+                clone.querySelector("[data-id='status']").textContent = `Status: ${character.status}`;
+                clone.querySelector("[data-id='species']").textContent = `Especie: ${character.species}`;
+                
+                
+                characterCardsContainer.appendChild(clone);
+            }
+        })
+        .catch(error => window.alert(`Error fetching characters: ${error}`));
+}
+
+function buscarNombre() {
+    const nombrePersonaje = document.getElementById('characterName').value.trim();
+    
+    if (nombrePersonaje === '') {
+        alert('Por favor ingrese un nombre de personaje.');
+        return; 
+    }
+
+    fetch(`https://rickandmortyapi.com/api/character/?name=${nombrePersonaje}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const characterCardsContainer = document.getElementById('characterCards');
+            const template = document.getElementById('characterCardTemplate');
+            
+            
+            characterCardsContainer.innerHTML = '';
+
+            
+            data.results.forEach(character => {
+                const clone = document.importNode(template.content, true);
+                
+               
+                clone.querySelector("[data-id='image']").src = character.image;
+                clone.querySelector("[data-id='title']").textContent = character.name;
+                clone.querySelector("[data-id='status']").textContent = `Status: ${character.status}`;
+                clone.querySelector("[data-id='species']").textContent = `Especie: ${character.species}`;
+                
+                
+                characterCardsContainer.appendChild(clone);
+            });
+        })
+        .catch(error => window.alert(`Error fetching characters: ${error}`));
+}
+
 
